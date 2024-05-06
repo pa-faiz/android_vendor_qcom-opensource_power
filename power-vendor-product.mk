7243274a681c764f873091716ce2616e14d39feb
quic_shabana <quic_shabana@quicinc.com>
2024-05-06 15:14:40 +0530

    Power: Add powerhint.xml for Matrix/niobe
    Added powerhint.xml for Matrix/niobe target
    
    Change-Id: I7f6f5b13bd924b263911f0054a6f043f12ad328e

--- power-vendor-product.mk
+++ power-vendor-product.mk
@@ -29,4 +29,6 @@ else ifeq ($(TARGET_BOARD_PLATFORM),pitti)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/pitti/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 else ifeq ($(TARGET_BOARD_PLATFORM),volcano)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/volcano/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+else ifeq ($(TARGET_BOARD_PLATFORM),niobe)
+PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/niobe/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 endif
